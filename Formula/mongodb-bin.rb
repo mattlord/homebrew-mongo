692be3dc02a5570fe4a8b03309847c670f43f2b6
Matt Lord <mattalord@gmail.com>
2019-02-04 13:10:01 -0500

    Testing out binary package installation since it doesn't seem like we can't use bottles

---
+++ Formula/mongodb-bin.rb
@@ -0,0 +1,14 @@
+class Mongodb < Formula
+  desc "High-performance, schema-free, document-oriented database"
+  homepage "https://www.mongodb.com/"
+  url "https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-4.0.5.tgz"
+  sha256 "3a7c5a60ab3b5d0614d44e64c02f3550ca1f639da1b618d2b566a0224108f67d"
+
+  def install
+    prefix.install Dir["*"]
+  end
+
+  test do
+    system "#{bin}/mongod", "--sysinfo"
+  end
+end
